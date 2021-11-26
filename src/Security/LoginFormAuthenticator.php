a30c4859a54ca75deb5bc215337dae87d893721b
ManuelGC4 <manuel_g.castilla10@hotmail.com>
2021-11-26 18:37:38 +0100

    pass correct cambiado

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -71,7 +71,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator implements P
 
         if (!$user) {
             // fail authentication with a custom error
-            throw new CustomUserMessageAuthenticationException('El correo o la contraseña que ha introducido no son correctas');
+            throw new CustomUserMessageAuthenticationException('El correo que ha introducido no es correcto');
         }
 
         return $user;
